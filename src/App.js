20335a84df6fc0ed02adac9280f7395bca3a5e3a
Muhammadrasulprojects <rasulgold1@gmail.com>
2023-11-01 20:02:24 +0500

    yuklash

--- src/App.js
+++ src/App.js
@@ -3,23 +3,43 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <div className="container">
+          <h1>Олег Марков</h1>
+          <ul>
+            <li><a href="#">Главная</a></li>
+            <li><a href="#" className='h'>Обо мне</a></li>
+            <li><a href="#" className='h'>Связаться</a></li>
+          </ul>
+        </div>
       </header>
+      <section>
+        <div className="container">
+          <div className="rasm1">
+            <img className='rasm11' src="https://static.wixstatic.com/media/d78df6_4517dc1e0b1545bf84dccd915203c870.jpg/v1/fill/w_378,h_378,al_c,q_80,usm_0.66_1.00_0.01/d78df6_4517dc1e0b1545bf84dccd915203c870.jpg" alt="" />
+            <img className='rasm11' src="https://static.wixstatic.com/media/d78df6_3eea4c192eed4667a602ead5652459c5.jpg/v1/fill/w_378,h_378,al_c,q_80,usm_0.66_1.00_0.01/d78df6_3eea4c192eed4667a602ead5652459c5.jpg" alt="" />
+            <img className='rasm11' src="https://static.wixstatic.com/media/d78df6_b9747effc3074458a295cf5674f8c73c.jpg/v1/fill/w_378,h_378,al_c,q_80,usm_0.66_1.00_0.01/d78df6_b9747effc3074458a295cf5674f8c73c.jpg" alt="" />
+          </div>
+          <div className="rasm1">
+            <img className='rasm10' src="https://static.wixstatic.com/media/d78df6_9f4f79dd27814710b261e3acb931156e.jpg/v1/fill/w_378,h_566,al_c,q_80,usm_0.66_1.00_0.01/d78df6_9f4f79dd27814710b261e3acb931156e.jpg" alt="" />
+            <img className='rasm10' src="https://static.wixstatic.com/media/d78df6_b572be78b8984e2db58978eb21e433d7.jpg/v1/fill/w_378,h_378,al_c,q_80,usm_0.66_1.00_0.01/d78df6_b572be78b8984e2db58978eb21e433d7.jpg" alt="" />
+            <img className='rasm10' src="https://static.wixstatic.com/media/d78df6_394a136048754355ad8432b30522c761.jpg/v1/fill/w_378,h_378,al_c,q_80,usm_0.66_1.00_0.01/d78df6_394a136048754355ad8432b30522c761.jpg" alt="" />
+          </div>
+          <div className="rasm1">
+            <img className='rasm11' src="https://static.wixstatic.com/media/d78df6_914d12d64a57420c99737b9933ebe7fc.jpg/v1/fill/w_378,h_378,al_c,q_80,usm_0.66_1.00_0.01/d78df6_914d12d64a57420c99737b9933ebe7fc.jpg" alt="" />
+            <img className='rasm11' src="https://static.wixstatic.com/media/d78df6_afe9335eb67640c988d648f7671308e5.jpg/v1/fill/w_378,h_378,al_c,q_80,usm_0.66_1.00_0.01/d78df6_afe9335eb67640c988d648f7671308e5.jpg" alt="" />
+            <img className='rasm11' src="https://static.wixstatic.com/media/d78df6_24dc2b5011604bc7ada79cbd953a7c5a.jpg/v1/fill/w_378,h_378,al_c,q_80,usm_0.66_1.00_0.01/d78df6_24dc2b5011604bc7ada79cbd953a7c5a.jpg" alt="" />
+          </div>
+        </div>
+      </section>
+      <footer>
+        <div className="container">
+          <p>© 2035 Олег Марков. Сайт создан на Wix.com.
+          </p>
+        </div>
+      </footer>
     </div>
   );
 }
-
 export default App;
